3ad83bbb0cbd36cc69864174080a031a0b729940
kimo-s <karim.ahmed1999@hotmail.com>
2020-11-10 22:44:53 -0500

    Added the basis of registering items and blocks. Added the Redstone reciver block. Added a tab for mod items only on createive mode

--- src/main/java/com/example/Tutils/Tutils.java
+++ src/main/java/com/example/Tutils/Tutils.java
@@ -28,8 +28,8 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("tutils")
+public class Tutils
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
