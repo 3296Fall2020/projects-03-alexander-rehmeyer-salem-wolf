943a8e78263a2353a41e0c0faf87b7e33508dd26
Alexander Rodriguez <tug98890@temple.edu>
2020-11-11 10:07:10 -0500

    Added the newly made Uranium tools to the item list

--- src/main/java/com/example/lists/ItemList.java
+++ src/main/java/com/example/lists/ItemList.java
@@ -5,6 +5,11 @@ import net.minecraft.item.Item;
 public class ItemList
 {
     public static Item uranium_ingot;
+    public static Item uranium_pickaxe;
+    public static Item uranium_sword;
+    public static Item uranium_axe;
+    public static Item uranium_shovel;
+    public static Item uranium_hoe;
 
     public static Item uranium_block;
 }
