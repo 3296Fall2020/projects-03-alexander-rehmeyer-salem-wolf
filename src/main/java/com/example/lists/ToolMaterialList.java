5edfc5f11e8d301420cf4d6e73e7919bcc146c7f
Alexander Rodriguez <tug98890@temple.edu>
2020-11-11 10:07:59 -0500

    Added the newly made material as something that the game can recognize as a material that a tool can use

--- src/main/java/com/example/lists/ToolMaterialList.java
+++ src/main/java/com/example/lists/ToolMaterialList.java
@@ -1,2 +1,60 @@
-package com.example.lists;public enum ToolMaterialList {
+package com.example.lists;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
+
+public enum ToolMaterialList implements IItemTier
+{
+    uranium(10.0f, 9.0f, 3000, 3, 25, ItemList.uranium_ingot);
+
+    private float attackDamage, efficiency;
+    private int durability, harvestLevel, enchantability;
+    private Item repairMaterial;
+
+    private ToolMaterialList(float attackDamage, float efficiency, int durability, int harvestLevel, int enchantability, Item repairMaterial)
+    {
+        this.attackDamage = attackDamage;
+        this.efficiency = efficiency;
+        this.durability = durability;
+        this.harvestLevel = harvestLevel;
+        this.enchantability = enchantability;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getMaxUses()
+    {
+        return this.durability;
+    }
+
+    @Override
+    public float getEfficiency()
+    {
+        return this.efficiency;
+    }
+
+    @Override
+    public float getAttackDamage()
+    {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel()
+    {
+        return this.harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability()
+    {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial()
+    {
+        return Ingredient.fromItems(this.repairMaterial);
+    }
 }
