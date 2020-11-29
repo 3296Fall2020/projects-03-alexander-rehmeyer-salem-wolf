86c836a8fc74dcdde7b4ec72c37798f4a79290da
Alexander Rodriguez <tug98890@temple.edu>
2020-11-29 16:27:42 -0500

    Registers each shovel-pickaxe multi-tool per material in-game

--- src/main/java/com/example/Tutils/ModItems.java
+++ src/main/java/com/example/Tutils/ModItems.java
@@ -1,10 +1,12 @@
 package com.example.Tutils;
 
 import com.example.Tutils.Items.RedstoneRemote;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.client.model.obj.MaterialLibrary;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -21,7 +23,16 @@ public class ModItems {
     public static final RegistryObject<ShovelItem> uranium_shovel = ITEMS.register("uranium_shovel", () -> new ShovelItem(ModMaterials.uranium, -3.0f, 6.0f, new Item.Properties().group(Tutils.TAB)));
     public static final RegistryObject<HoeItem> uranium_hoe = ITEMS.register("uranium_hoe", () -> new HoeItem(ModMaterials.uranium, 1, 6.0f, new Item.Properties().group(Tutils.TAB)));
     public static final RegistryObject<Item> redstone_remote = ITEMS.register("redstone_remote", RedstoneRemote::new);
-  
+
+    // Multi-Tools
+    public static final RegistryObject<ItemShovelPickaxe> wooden_shovel_pickaxe = ITEMS.register("wooden_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.wood, -2, 6, new Item.Properties().group(Tutils.TAB)));
+    public static final RegistryObject<ItemShovelPickaxe> stone_shovel_pickaxe = ITEMS.register("stone_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.stone, -2, 6, new Item.Properties().group(Tutils.TAB)));
+    public static final RegistryObject<ItemShovelPickaxe> iron_shovel_pickaxe = ITEMS.register("iron_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.iron, -2, 6, new Item.Properties().group(Tutils.TAB)));
+    public static final RegistryObject<ItemShovelPickaxe> gold_shovel_pickaxe = ITEMS.register("gold_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.gold, -2, 6, new Item.Properties().group(Tutils.TAB)));
+    public static final RegistryObject<ItemShovelPickaxe> diamond_shovel_pickaxe = ITEMS.register("diamond_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.diamond, -2, 6, new Item.Properties().group(Tutils.TAB)));
+    public static final RegistryObject<ItemShovelPickaxe> uranium_shovel_pickaxe = ITEMS.register("uranium_shovel_pickaxe", () -> new ItemShovelPickaxe(ModMaterials.uranium, -2, 6, new Item.Properties().group(Tutils.TAB)));
+
+
     // Block items
     public static final RegistryObject<Item> uranium_block = ITEMS.register("uranium_block", () -> new BlockItem(ModBlocks.uranium_block.get(), new Item.Properties().group(Tutils.TAB)));
     public static final RegistryObject<Item> uranium_ore = ITEMS.register("uranium_ore", () -> new BlockItem(ModBlocks.uranium_ore.get(), new Item.Properties().group(Tutils.TAB)));
